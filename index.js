17f71faa05c293e4df834f3b6eab60df925abeb2
iwint <iwint.cseasnsct@gmail.com>
2022-12-08 09:01:21 +0530

    New Changes

--- index.js
+++ index.js
@@ -1,51 +1,46 @@
-const express = require("express")
-const { appendFile } = require("fs")
-const mongoose = require("mongoose")
-const engine = require("ejs-mate")
-const path = require("path")
-var methodOverride = require('method-override')
+const express = require('express');
+const { appendFile } = require('fs');
+const mongoose = require('mongoose');
+const engine = require('ejs-mate');
+const path = require('path');
+var methodOverride = require('method-override');
 
 const app = express();
-mongoose.connect('mongodb://127.0.0.1:27017/').then(()=> console.log("connection opened with mongoose")).catch((err)=> console.log("cannot access mongodb for some reason"))
+mongoose
+  .connect('mongodb://127.0.0.1:27017/')
+  .then(() => console.log('connection opened with mongoose'))
+  .catch((err) => console.log('cannot access mongodb for some reason'));
 app.engine('ejs', engine);
- 
+
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
-app.use(express.static('public'))
+app.set('view engine', 'ejs');
+app.use('/views', express.static(__dirname + '/views'));
 
+app.use(express.static('public'));
 
-app.use(methodOverride('_method'))
-
-
+app.use(methodOverride('_method'));
 
 app.listen(3000, () => {
-    console.log("the connection is opened on port 3000")
-})
-
+  console.log('the port is opened on 3000');
+});
 
 app.get('/', (req, res, next) => {
-    res.render('./boilerplate/home')
-})
-
+  res.render('./boilerplate/home');
+});
 
 app.get('/admin', (req, res, next) => {
-    
-    var name = { name : ' gratus', namee2: '' }
-    console.log(name.name)
-    res.render('admin', {name})
-})
+  var name = { name: ' gratus', namee2: '' };
+  console.log(name.name);
+  res.render('admin', { name });
+});
 
-
-app.post('/admin', (req, res, next) => {
-    
-})
+app.post('/admin', (req, res, next) => {});
 
 app.put('/resource', (req, res, next) => {
-    console.log("the method over ride is working")
-    res.send("this is working")
-})
-
+  console.log('the method over ride is working');
+  res.send('this is working');
+});
 
 ////////
-
